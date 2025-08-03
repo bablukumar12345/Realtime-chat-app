69c30355cd638333418cae07d0dd89073c256e50
Bablu Kumar <babluk22850@gmail.com>
2025-08-03 19:20:37 +0530

    modify file structure

--- Nodeserver/index.js
+++ Nodeserver/index.js
@@ -1,7 +1,54 @@
-const io = require("socket.io")(8000, {
+// const io = require("socket.io")(8000, {
+//     cors: {
+//         origin: "*",
+//     }
+// });
+
+// const users = {};
+
+// io.on("connection", socket => {
+//     socket.on('New-User-Joined', name => {
+//         console.log('New user:', name);
+//         users[socket.id] = name;
+//         socket.broadcast.emit('User-Joined', name);
+//     });
+
+//     socket.on('Send', message => {
+//         socket.broadcast.emit('receive', {
+//             message: message,
+//             name: users[socket.id]
+//         });
+//     });
+
+//     socket.on('disconnect', () => {
+//         const username = users[socket.id];
+//         console.log("User disconnected:", username);
+//         if (username) {
+//             socket.broadcast.emit('left', username);
+//             delete users[socket.id];
+//         }
+//     });
+// });
+
+
+
+
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
     cors: {
-        origin: "*",
+        origin: "*"
     }
+});
+const path = require("path");
+
+// Serve static files from current directory
+app.use(express.static(__dirname));
+
+// Serve index.html on "/"
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const users = {};
@@ -28,4 +75,8 @@ io.on("connection", socket => {
             delete users[socket.id];
         }
     });
+});
+
+http.listen(8000, () => {
+    console.log("Server running on port 8000");
 });
